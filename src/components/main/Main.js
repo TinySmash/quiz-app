6d9fbcc36713184e8b1da1baa1262df32210e998
Islam Elgueniari <islam.gueniari@gmail.com>
2022-12-02 15:20:38 +0100

    Interface

---
+++ src/components/main/Main.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Main.css';
+
+export default function Main() {
+  return (
+    <div className='mainPage'>
+      <h1 className='mainTitle'>Test your Tech knowledge <br />in this <span className='appName'> Quiz app </span></h1>
+      <h3 className="subtitle">With a lot of diversified questions about </h3>
+      <ul className="subjects">
+        <li className="subject-items">
+          <h1 className="subject-titles">Javascript</h1>
+        </li>
+        <li className="subject-items">
+          <h1 className="subject-titles">Coding</h1>
+        </li>
+        <li className="subject-items">
+         <h1 className="subject-titles">Linux</h1>
+        </li>
+      </ul>
+    </div>
+  )
+}
