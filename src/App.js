fbaf021265e4f76f21d4e7699d0b6b625ec7e03b
Islam Elgueniari <islam.gueniari@gmail.com>
2022-12-02 10:09:51 +0100

    Title

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
 import React from 'react'
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Main from './components/main/Main';
 
 function App() {
   return (
     <>
-      <nav className='navbar'>
-        <ul className="menu">
-          <li>
-            <button className="menu-item" id='first-item'>
-          </button>
-          </li>
-          <li>
-            <button className="menu-item" id='second-item'>
+      <Router>
+        <nav className='navbar'>
+          <ul className="menu">
+            <li>
+              <button className="menu-item" id='first-item'>
+                <img src="menu-alt-1.png" alt="" className='icons' />
             </button>
-          </li>
-          <li>
-          <button className="menu-item" id='third-item'>
-          </button>
-          </li>
-          <li>
-          <button className="menu-item" id='fourth-item'>
-          </button>
-          </li>
-          
-          
-          
+            </li>
+            <li>
+              <button className="menu-item" id='second-item'>
+                <img src="question-mark-circle.png" alt="" className='icons' />
+              </button>
+            </li>
+            <li>
+            <button className="menu-item" id='third-item'>
+              <img src="plus.png" alt="" className='icons' />
+            </button>
+            </li>
+            <li>
+            <button className="menu-item" id='fourth-item'>
+              <img src="star.png" alt="" className='icons' />
+            </button>
+            </li>
+          </ul>
+        </nav>
 
-        </ul>
-      </nav>
+        <Routes>
+          <Route exact path='/' element={<Main/>}></Route>
+        </Routes>
+
+      </Router>
     </>
   );
 }
